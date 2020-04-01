6ca36d4e565234be139f361b47506b156484c688
besseddrest <hcabalic@gmail.com>
2020-03-31 23:39:42 -0700

    add json for master & main_release response, get tags from Serato

---
+++ update.py
@@ -0,0 +1,22 @@
+tags = [
+    "added",
+    "album",
+    "artist",
+    "bitrate",
+    "bpm",
+    "comment",
+    "composer",
+    "filename",
+    "genre",
+    "grouping",
+    "key",
+    "label",
+    "length",
+    "location",
+    "remixer",
+    "samplerate",
+    "size",
+    "track"
+    "video track",
+    "year"
+]
